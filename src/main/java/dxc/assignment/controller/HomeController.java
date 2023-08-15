04d9ec31303d93a4abedc33962ebd076757850a7
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-08-15 17:55:22 +0700

    Initial commit

---
+++ src/main/java/dxc/assignment/controller/HomeController.java
@@ -0,0 +1,16 @@
+package dxc.assignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping("/MemberManagement")
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "index";
+	}
+}
